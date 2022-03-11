3b9ac67fac06539928f5bee348095b7b4722f90f
ichal wira <ichalwiradev@gmail.com>
2022-03-11 09:35:00 +0700

    Services

--- lib/services/auth/Auth_services.dart
+++ lib/services/auth/Auth_services.dart
@@ -1,9 +1,15 @@
+import 'package:final_project_bootcampflutter/ui/auth_ui/login_screen_resto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../../ui/home_screen_resto.dart';
 
 class AuthServices with ChangeNotifier {
+  bool _isLoading = false;
+// var newPassword = "";
+  String? _errorMassage;
+  bool get isLoading => _isLoading;
+  String get pesanError => _errorMassage!;
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   registerSubmit(TextEditingController? _emailController,
@@ -47,5 +53,49 @@ class AuthServices with ChangeNotifier {
         ),
       );
     }
+  }
+
+  resetPassword(BuildContext context, var email) async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.orangeAccent,
+          content: Text(
+            'Password Reset Email has been sent !',
+            style: TextStyle(fontSize: 18.0),
+          ),
+        ),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: Colors.orangeAccent,
+            content: Text(
+              'No user found for that email.',
+              style: TextStyle(fontSize: 18.0),
+            ),
+          ),
+        );
+      }
+    }
+  }
+
+  Future signOut(BuildContext ctx) async {
+    await _firebaseAuth.signOut().then((value) => Navigator.of(ctx)
+        .pushReplacement(
+            MaterialPageRoute(builder: (context) => LoginScreen())));
+  }
+
+  void setisLoading(val) {
+    _isLoading = val;
+    notifyListeners();
+  }
+
+  void setMassage(massage) {
+    _errorMassage = massage;
+    notifyListeners();
   }
 }
