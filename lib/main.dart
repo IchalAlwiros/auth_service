0d9f20bfa2f02d6079e00e38d312451266372931
ichal wira <ichalwiradev@gmail.com>
2022-03-11 09:35:35 +0700

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:final_project_bootcampflutter/services/api/ApiServices.dart';
 import 'package:final_project_bootcampflutter/services/auth/Auth_services.dart';
 import 'package:final_project_bootcampflutter/services/theme_provider/theme_provider.dart';
 import 'package:final_project_bootcampflutter/ui/home_screen_resto.dart';
-import 'package:final_project_bootcampflutter/ui/login_screen_resto.dart';
+import 'package:final_project_bootcampflutter/ui/auth_ui/login_screen_resto.dart';
 import 'package:final_project_bootcampflutter/ui/splash_screen_resto.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
